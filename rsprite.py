80f484842e86187d6c115c2fd06c81b1f457b70a
Rob Righter <robrighter@gmail.com>
2017-02-21 01:36:52 +0000

    added movement

--- rsprite.py
+++ rsprite.py
@@ -8,6 +8,16 @@ class RSprite(pygame.sprite.Sprite):
 		self.surf = pygame.Surface((75, 25))
 		self.surf.fill((255, 255, 255))
 		self.rect = self.surf.get_rect()
+	
+	def update(self, pressed_keys):
+		if pressed_keys[K_UP]:
+			self.rect.move_ip(0, -5)
+		if pressed_keys[K_DOWN]:
+			self.rect.move_ip(0, 5)
+		if pressed_keys[K_LEFT]:
+			self.rect.move_ip(-5, 0)
+		if pressed_keys[K_RIGHT]:
+			self.rect.move_ip(5, 0)
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
