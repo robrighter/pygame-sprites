fd8a5f9b51c789f1a5ff90c31f32e6a1e25765cd
Rob Righter <robrighter@gmail.com>
2017-02-21 01:29:25 +0000

    r

--- rsprite.py
+++ rsprite.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 
 class RSprite(pygame.sprite.Sprite):
 	def __init__(self):
-		super(Player, self).__init__()
+		super(RSprite, self).__init__()
 		self.surf = pygame.Surface((75, 25))
 		self.surf.fill((255, 255, 255))
 		self.rect = self.surf.get_rect()
