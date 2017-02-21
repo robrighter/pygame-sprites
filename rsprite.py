9c45b99217179f48199c6f1d9a22c27242078ecb
Rob Righter <robrighter@gmail.com>
2017-02-20 20:15:02 -0500

    Update rsprite.py

--- rsprite.py
+++ rsprite.py
@@ -1,1 +1,26 @@
 
+import pygame
+from pygame.locals import *
+
+class RSprite(pygame.sprite.Sprite):
+	def __init__(self):
+		super(Player, self).__init__()
+		self.surf = pygame.Surface((75, 25))
+		self.surf.fill((255, 255, 255))
+		self.rect = self.surf.get_rect()
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+player = RSprite()
+
+running = True
+
+while running:
+	for event in pygame.event.get():
+		if event.type == KEYDOWN:
+			if event.key == K_ESCAPE:
+				running = False
+		elif event.type == QUIT:
+			running = False
+	screen.blit(player.surf, (400, 300))
+	pygame.display.flip()
